295406b74cbba3c9d8909b836ebcb3baf2e6e432
Matt Smith <matthew.smith491@gmail.com>
2016-06-15 14:39:40 -0700

    Adding functional solution for problem one

--- one.nim
+++ one.nim
@@ -1,3 +1,5 @@
+from sequtils import toSeq, filter, foldl
+
 proc mult_3_and_5(num: int): int =
     var sum = 0;
     for elem in 3..(num - 1):
@@ -5,5 +7,13 @@ proc mult_3_and_5(num: int): int =
             sum += elem
     return sum
 
+proc mult_3_and_5_b(num: int): int =
+    result = toSeq(1..(num - 1))
+        .filter(
+            proc(x: int): bool = x mod 3 == 0 or x mod 5 == 0)
+        .foldl(a + b)
+
 var answer : int = mult_3_and_5(1000)
+echo(answer)
+answer = mult_3_and_5_b(1000)
 echo(answer)
