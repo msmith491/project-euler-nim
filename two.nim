71dd411a598e043aa0c212fca30a4b28e8432607
Matt Smith <matthew.smith491@gmail.com>
2016-06-16 14:32:58 -0700

    Updating problem_2 with arbitrary precision solution

--- two.nim
+++ two.nim
@@ -1,32 +1,46 @@
-proc fib(n: int): int =
-    if n == 0: return 0
-    if n == 1: return 1
+import bigints
+from sequtils import toSeq, filter, map, foldl
+
+proc fib[T](n: T): BigInt =
+    if n == 0: return 1.initBigInt
+    if n == 1: return 1.initBigInt
 
     var
-        prev: int = 1
-        prev2: int = 0
-        r: int = 2
+        prev = 1.initBigInt
+        prev2 = 0.initBigInt
+        r = 2.initBigInt
 
-    for elem in 2..(n+1):
+    for elem in (2.initBigInt)..(n.initBigInt+1):
         r = prev + prev2
         prev2 = prev
         prev = r
 
-    return r
+    r
 
-proc fib_sum_even_below(n: int): int =
+# Super slow with bigints library
+proc fibr[T](n: T): BigInt =
+    let strn = $n
+    case strn
+    of "0":
+        1.initBigInt
+    of "1", "2":
+        n.initBigInt
+    else:
+        fibr(n - 1) + fibr(n - 2)
+
+
+proc fib_sum_even_below[T](n: T): BigInt =
     var tmp: int = 0
-    result = tmp
+    result = tmp.initBigInt
     var
-        num: int = 1
-        current: int = 0
-        found = newSeq[int]()
-    while current < n:
-        current = fib(num)
-        if current %% 2 == 0:
+        num = 1.initBigInt
+        current = 0.initBigInt
+        found = newSeq[BigInt]()
+    while current < n.initBigInt:
+        current = fibr(num)
+        if current mod 2.initBigint == 0:
             found.add(current)
             result += current
         num += 1
 
-echo(fib_sum_even_below(4000000))
-
+echo fib_sum_even_below(4000000)
