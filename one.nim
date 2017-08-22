2a6405369d177c3dd198bbdaed126d7078a79a0e
Matt Smith <matthew.smith491@gmail.com>
2017-08-22 17:33:52 -0500

    Updating gitignore and adding another solution to problem 1

--- one.nim
+++ one.nim
@@ -1,19 +1,15 @@
-from sequtils import toSeq, filter, foldl
+import sequtils
+import future
 
-proc mult_3_and_5(num: int): int =
-    var sum = 0;
-    for elem in 3..(num - 1):
-        if elem mod 3 == 0 or elem mod 5 == 0:
-            sum += elem
-    return sum
+proc mult_3_5(num: int): int =
+  var i: int = 3
+  while i < num:
+    if i mod 3 == 0 or i mod 5 == 0:
+      result += i
+    i += 1
 
-proc mult_3_and_5_b(num: int): int =
-    result = toSeq(1..(num - 1))
-        .filter(
-            proc(x: int): bool = x mod 3 == 0 or x mod 5 == 0)
-        .foldl(a + b)
+proc mult_3_5b(num: int): int =
+  toSeq(1..num-1).filter(x => x mod 3 == 0 or x mod 5 == 0).foldl(a + b)
 
-var answer : int = mult_3_and_5(1000)
-echo(answer)
-answer = mult_3_and_5_b(1000)
-echo(answer)
+echo mult_3_5(1000)
+echo mult_3_5b(1000)
